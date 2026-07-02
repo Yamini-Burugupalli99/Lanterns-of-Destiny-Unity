80addd7e5bcda1ec9bcbe43fc57cdd34e801033b
Yamini Burugupalli <burugupalliyaminidevi@gmail.com>
2026-07-02 18:10:41 +0530

    imported a bow and arrow model from asset store unity and placed them in player hand and all states works properly after fixing errors ,and created a avatar mask for jump steadily while aiming,and imported the new jump and aim and shoot states from mixamo

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -6,6 +6,7 @@ public class PlayerMovement : MonoBehaviour
     [Header("Movement")]
     public float walkSpeed = 3f;
     public float runSpeed = 6f;
+    public float aimSpeed = 2f;
     public float rotationSpeed = 10f;
 
     [Header("Jump")]
@@ -16,6 +17,10 @@ public class PlayerMovement : MonoBehaviour
     public Transform groundCheck;
     public LayerMask groundMask;
     public float groundDistance = 0.3f;
+
+    [Header("Aim")]
+    public GameObject crosshair;
+    public bool isAiming;
 
     private CharacterController controller;
     private Animator animator;
@@ -29,25 +34,41 @@ public class PlayerMovement : MonoBehaviour
         controller = GetComponent<CharacterController>();
         animator = GetComponent<Animator>();
         cam = Camera.main.transform;
+
+        if (crosshair != null)
+            crosshair.SetActive(false);
     }
 
     void Update()
     {
+        Aim();
+
         GroundCheck();
+
         Move();
+
         Jump();
+
+        FloorGrab();
+
+        WallGrab();
+
         ApplyGravity();
+
+        Shoot();
     }
 
     void GroundCheck()
     {
         isGrounded = Physics.CheckSphere(
-            groundCheck.position,
-            groundDistance,
-            groundMask);
+        groundCheck.position,
+        groundDistance,
+        groundMask);
+
+        animator.SetBool("Grounded", isGrounded);
 
         if (isGrounded && velocity.y < 0)
-            velocity.y = -2f;
+        velocity.y = -2f;
     }
 
     void Move()
@@ -55,50 +76,75 @@ public class PlayerMovement : MonoBehaviour
         float h = Input.GetAxisRaw("Horizontal");
         float v = Input.GetAxisRaw("Vertical");
 
-        Vector3 direction = new Vector3(h,0,v).normalized;
+        Vector3 direction = new Vector3(h, 0, v).normalized;
 
-        if(direction.magnitude >= 0.1f)
+        if (direction.magnitude >= 0.1f)
         {
             float targetAngle =
-                Mathf.Atan2(direction.x,direction.z) * Mathf.Rad2Deg
+                Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg
                 + cam.eulerAngles.y;
 
-            float angle = Mathf.LerpAngle(
-                transform.eulerAngles.y,
-                targetAngle,
-                rotationSpeed * Time.deltaTime);
+            if (isAiming)
+            {
+                transform.rotation = Quaternion.Lerp(
+                    transform.rotation,
+                    Quaternion.Euler(0, cam.eulerAngles.y, 0),
+                    rotationSpeed * Time.deltaTime);
+            }
+            else
+            {
+                float angle = Mathf.LerpAngle(
+                    transform.eulerAngles.y,
+                    targetAngle,
+                    rotationSpeed * Time.deltaTime);
 
-            transform.rotation =
-                Quaternion.Euler(0,angle,0);
+                transform.rotation = Quaternion.Euler(0, angle, 0);
+            }
 
             Vector3 moveDir =
-                Quaternion.Euler(0,targetAngle,0)
-                * Vector3.forward;
+                Quaternion.Euler(0, targetAngle, 0) * Vector3.forward;
 
-            float speed = Input.GetKey(KeyCode.LeftShift)
-                ? runSpeed
-                : walkSpeed;
+            float speed;
+
+            if (isAiming)
+            {
+                speed = aimSpeed;
+            }
+            else if (Input.GetKey(KeyCode.LeftShift))
+            {
+                speed = runSpeed;
+            }
+            else
+            {
+                speed = walkSpeed;
+            }
 
             controller.Move(moveDir.normalized * speed * Time.deltaTime);
 
-            animator.SetFloat(
-                "speed",
-                Input.GetKey(KeyCode.LeftShift)
-                ? 1f
-                : 0.5f);
+            if (isAiming)
+            {
+                animator.SetFloat("speed", 0.5f);
+            }
+            else if (Input.GetKey(KeyCode.LeftShift))
+            {
+                animator.SetFloat("speed", 1f);
+            }
+            else
+            {
+                animator.SetFloat("speed", 0.5f);
+            }
         }
         else
         {
-            animator.SetFloat("speed",0);
+            animator.SetFloat("speed", 0f);
         }
     }
 
     void Jump()
     {
-        if(Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
-            velocity.y =
-                Mathf.Sqrt(jumpHeight * -2f * gravity);
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
 
             animator.SetTrigger("Jump");
         }
@@ -107,6 +153,41 @@ public class PlayerMovement : MonoBehaviour
     void ApplyGravity()
     {
         velocity.y += gravity * Time.deltaTime;
+
         controller.Move(velocity * Time.deltaTime);
+    }
+
+    void Aim()
+    {
+        isAiming = Input.GetMouseButton(1);
+
+        animator.SetBool("isAim", isAiming);
+
+        if (crosshair != null)
+            crosshair.SetActive(isAiming);
+    }
+
+    void Shoot()
+    {
+       if (isAiming && Input.GetMouseButtonDown(0))
+       {
+           animator.SetTrigger("shoot");
+       } 
+    }
+
+    void FloorGrab()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            animator.SetTrigger("GrabFloor");
+        }
+    }
+
+    void WallGrab()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            animator.SetTrigger("GrabWall");
+        }
     }
 }
